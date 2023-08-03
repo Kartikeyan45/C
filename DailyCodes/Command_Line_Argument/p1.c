8d581abba9261e88fe3de846165cdcfd17bb45e7
Kartikeyan45 <92666318+Kartikeyan45@users.noreply.github.com>
2023-08-03 12:01:58 +0530

    Add files via upload

---
+++ DailyCodes/Command_Line_Argument/p1.c
@@ -0,0 +1,17 @@
+/*	Program 1 :
+*/
+
+#include<stdio.h>
+
+void main(int argc, char* argv[]){
+
+	printf("%s\n", argv[0]);
+
+	printf("%s\n", argv[1]);
+
+	printf("%s\n", argv[2]);
+
+	puts(argv[0]);
+
+	printf("%d\n", argc);
+}
---
+++ DailyCodes/Command_Line_Argument/p2.c
@@ -0,0 +1,12 @@
+/*	Program 2 :
+*/
+
+#include<stdio.h>
+
+void main(int argc, char* argv[]){
+
+	for(int i = 0; i < argc; i++){
+	
+		printf("%s\n", argv[i]);
+	}
+}
